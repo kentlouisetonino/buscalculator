d379c3f5ffddc811f41c8ddc2e4b649aa379c98e
Kent Louise Tonino <tonino.kentlouise@gmail.coM>
2024-07-11 05:18:20 +0800

    Clear screen.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,10 @@
 #include "./displays//app_description.h"
+#include "./displays/clear_screen.h"
 #include <stdio.h>
 
 int main(void) {
+  // Clean up the screen.
+  clear_screen();
   app_description();
 
   return 0;
